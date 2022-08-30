75b919737c629dc2ab1100e9e9974f9a956ad8da
DevExpressExampleBot <fakeEmail>
2022-08-30 09:50:38 +0400

    Source auto update [skip ci]

--- VB/SeriesItemTemplateSample/Model/GdpSeries.vb
+++ VB/SeriesItemTemplateSample/Model/GdpSeries.vb
@@ -1,16 +1,20 @@
-﻿Imports System
 Imports System.Collections.Generic
 
 Namespace SeriesItemTemplateSample.Model
-    #Region "#Model"
+
+#Region "#Model"
     Public Class GdpSeries
-        Public Property CountryName() As String
-        Public Property Values() As IEnumerable(Of Gdp)
+
+        Public Property CountryName As String
+
+        Public Property Values As IEnumerable(Of Gdp)
     End Class
 
     Public Class Gdp
-        Public Property Year() As Integer
-        Public Property Value() As Double
+
+        Public Property Year As Integer
+
+        Public Property Value As Double
     End Class
-    #End Region ' #Model
+#End Region  ' #Model
 End Namespace
